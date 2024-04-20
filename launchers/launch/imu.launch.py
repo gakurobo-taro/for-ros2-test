2edb3e3dd038e0b94a59ad046a78bcb0d6b5d206
naga <toritonsuibu0527@gmail.com>
2024-04-21 03:33:11 +0900

    add uart_node

--- launchers/launch/imu.launch.py
+++ launchers/launch/imu.launch.py
@@ -3,6 +3,17 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+            Node(
+                package='uart_communicator',
+                executable='main',
+                exec_name='uart_communicator',
+                name='uart_communicator',
+                output='screen',
+                emulate_tty=True,
+                parameters=[
+                    {"serial_name": "/dev/ttyACM0"}
+                ]
+            ),
         Node(
             package='imu_reader',
             executable='publisher',
